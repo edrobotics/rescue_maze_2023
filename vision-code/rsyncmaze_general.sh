2e1e70ee01a174b9423358c1b0417ff495110c1a
apollen85 <123020764+apollen85@users.noreply.github.com>
2023-06-25 20:14:36 +0200

    updated rsyncmaze.sh to be more general (rsyncmaze_general.sh)

--- vision-code/rsyncmaze_general.sh
+++ vision-code/rsyncmaze_general.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/bash
 git pull 
-rsync -a --progress --exclude 'log' "main-code"  ./ pi@raspberrypi.local://home/pi/rescue_maze_2023/vision-code/
-rsync -a --progress --exclude 'log' "main-code"  ../maze-code/ pi@raspberrypi.local://home/pi/rescue_maze_2023/maze-code/
+rsync -a --progress --exclude 'log' ./ pi@raspberrypi.local://home/pi/rescue_maze_2023/vision-code/
+rsync -a --progress ../maze-code/ pi@raspberrypi.local://home/pi/rescue_maze_2023/maze-code/
